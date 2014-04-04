4c3d5acebcf3aa1180499b15ac40035908b7433b
Sahab Yazdani <syazdani@vastcap.com>
2014-04-04 10:50:11 -0400

    Add documentation.

--- OsTicket.API/Attachment.cs
+++ OsTicket.API/Attachment.cs
@@ -5,10 +5,22 @@ using System.Text;
 
 namespace OsTicket.API
 {
+    /// <summary>
+    /// Represents an Attachment in the OsTicket system
+    /// </summary>
     public class Attachment
     {
+        /// <summary>
+        /// The Filename of the Attachment
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// The Content MIME Type of the Attachment
+        /// </summary>
         public string Type { get; set; }
+        /// <summary>
+        /// The actual byte content of the Attachment
+        /// </summary>
         public byte[] Content { get; set; }
     }
 }
--- OsTicket.API/Ticket.cs
+++ OsTicket.API/Ticket.cs
@@ -6,22 +6,82 @@ using System.Text;
 
 namespace OsTicket.API
 {
+    /// <summary>
+    /// Represents a Ticket in the OsTicket system
+    /// </summary>
     public class Ticket
     {
+        /// <summary>
+        /// Constructs a new Ticket
+        /// </summary>
         public Ticket()
         {
             Attachments = new List<Attachment>();
             ExtraFields = new Dictionary<string, object>();
         }
 
+        /// <summary>
+        /// The e-mail address of the user submitting the ticket. This
+        /// value must parse into an e-mail address otherwise OsTicket will
+        /// throw an exception on submission
+        /// </summary>
         public string Email { get; set; }
+
+        /// <summary>
+        /// The name of the user submitting the ticket.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// A summary of the issue that the user is facing
+        /// </summary>
         public string Subject { get; set; }
+
+        /// <summary>
+        /// A description of the issue that the user is facing.
+        /// If IsMessageHtml is true, then this field is treated as HTML
+        /// </summary>
         public string Message { get; set; }
+
+        /// <summary>
+        /// Whether or not to treat the Message property as an HTML fragment or plain text.
+        /// By default, it is treated as Plain Text.
+        /// </summary>
         public bool IsMessageHtml { get; set; }
-        public int TopicId { get; set; }
-        public int PriorityId { get; set; }
+
+        /// <summary>
+        /// The IP Address of the user sending the message.
+        /// While this is not a required property, OsTicket can be configured to blacklist
+        /// users based on their IP Address.
+        /// </summary>
+        public string IPAddress { get; set; }
+
+        /// <summary>
+        /// The Help Topic Id of the Ticket.
+        /// </summary>
+        /// <remarks>
+        /// OsTicket does not expose this value by default, but it can be found via inspection of the HTML
+        /// or via the API extensions provided by this library.
+        /// </remarks>
+        public int? TopicId { get; set; }
+
+        /// <summary>
+        /// The Priority Id of the Ticket
+        /// </summary>
+        /// <remarks>
+        /// OsTicket does not expose this value by default, but it can be found via inspection of the HTML
+        /// or via the API extensions provided by this library.
+        /// </remarks>
+        public int? PriorityId { get; set; }
+
+        /// <summary>
+        /// The list of attachments to add to this Ticket
+        /// </summary>
         public List<Attachment> Attachments { get; private set; }
+
+        /// <summary>
+        /// The list of extra fields to populate with this ticket. The keys are specified in the OsTicket UI
+        /// </summary>
         public Dictionary<string, object> ExtraFields { get; private set; }
     }
 }
